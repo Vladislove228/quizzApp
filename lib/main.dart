9e02aa896a4058613285cff8140a50a0805a1d57
Vladislove228 <wlad.151@mail.ru>
2023-12-15 21:23:24 +0100

    Question Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/quiz.dart';
 
-import 'package:quizz_app/start_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.deepPurple,
-                Colors.purple,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen( this.startQuiz,{super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -9,7 +11,10 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset("assets/images/quiz-logo.png", width: 300),
+          Opacity(
+            opacity: 0.6,
+            child: Image.asset("assets/images/quiz-logo.png", width: 300),
+          ),
           const SizedBox(height: 80),
           const Text(
             "Lern flutter the funway",
@@ -19,12 +24,13 @@ class StartScreen extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 80),
-          OutlinedButton(
-            onPressed: () {},
+          OutlinedButton.icon(
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
-            child: Text("Start Quizz"),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text("Start Quizz"),
           ),
         ],
       ),
